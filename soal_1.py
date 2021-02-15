06c6b1611bdc64546dd7de5dd1309d9b7ad572c4
Adiatmaja <78332540+Adiatmaja@users.noreply.github.com>
2021-02-15 21:00:50 +0700

    Add files via upload

---
+++ soal_1.py
@@ -0,0 +1,25 @@
+'''
+Eren telah memulai usaha sauna pada tahun 2019. Pada awal dibukanya usaha, usaha Eren berjalan dengan lancar.
+pendapatan per bulannya ialah Rp. 25000000
+Sedangkan Biaya air, listrik, perawatan dan gaji karyawan ialah Rp. 10000000 per bulannya
+Namun, pada tahun 2020 dan setelahnya, pendapatan usaha Eren berkurang dikarenakan adanya pandemi yang mengakibatkan berkurang nya pelanggan.
+Kerugian yang dialami usaha Eren berkurang hingga 25%, Hitunglah total pendapatan usaha Eren 2 tahun setelah dibukanya usaha Eren!
+'''
+# Input User
+a = 25000000 #int(input("Masukkan pendapatan per bulan = "))          # Pendapatan
+b = 10000000 #int(input("Masukkan maintenance cost per bulan = "))    # Maintenance Cost
+c = 2 #int(input("Berapa tahun yang anda ingin hitung? "))            # Tahun
+d = c * 12                                                            # Jumlah Bulan
+
+b1 = b * d                                                            # Total maintenance cost
+
+a1 = 0                                                                # Sebagai tempat untuk menampung nilai pendapatan
+for i in range (d):                                                   # For akan berjalan sebanyak jumlah tahun dikali 12
+    if i < 12:
+        a1 = a1 + a                                                   # Apabila masih di bulan 1 - 12, nilai akan ditambahkan ke tampungan
+    else:
+        a1 = a1 + (a * 0.75)                                          # Apabila sudah di bulan 12+, nilai yang akan ditambahkan ke tampungan sebelumnya dikurangi 25% terlebih dahulu
+
+total = a1 - b1
+
+print ("Total pendapatan selama", c, "Tahun adalah = Rp.", total)
