64c38b46b95cfe9790e220bb7be24efaa01b2b15
Adiatmaja <78332540+Adiatmaja@users.noreply.github.com>
2021-03-09 18:52:31 +0700

    Update soal_4.py

--- soal_4.py
+++ soal_4.py
@@ -1,62 +1,35 @@
 ''' Struktur Kontrol Perulangan
 Buatlah sebuah program yang dapat menginputkan angka secara terus menerus apabila user menginputkan 0 maka proses input berhenti,
-menuju ke menu dan meminta user apakah user ingin menambahkan nilai lagi, menampilkan rata - rata dari semua input,
-jumlah bilangan genap berdasarkan input atau jumlah bilangan ganjil berdasarkan input, kemudian ada pilihan exit.
-
-Input :
-b = Pemilihan menu
-a = Memasukkan nilai ke data
-
-Proses dan Output :
-Membuat variabel n sebagai list
-Menampilkan menu
-Jika b = 1 maka user diminta menambahkan nilai ke data
-    input a dengan perulangan
-Jika b = 2 maka akan menampilkan rata rata dari data
-    d = jumlah nilai / jumlah data
-    print (d)
-Jika b = 3 maka akan menampilkan jumlah bilangan genap
-    jika data % 2 = 0
-        e += 1
-        print (e)
-Jika b = 4 maka akan menampilkan jumlah bilangan ganjil
-    jika data % 2 = 1
-        f += 1
-        print (f)
-Jika b = 5 maka program akan berhenti
+menuju ke menu dan meminta user apakah user ingin menambahkan nilai lagi, menampilkan jumlah bilangan genap berdasarkan input atau
+jumlah bilangan ganjil berdasarkan input, kemudian ada pilihan exit.
 '''
 
 n = []
-e = 0
-f = 0
+c = 0
+d = 0
 
 while True:
-    print ("MENU : \n1. Input Nilai\n 2. Rata - rata\n3. Bilangan genap\n4. Bilangan ganjil\n5. Exit")
-    b = int(input("Masukkan pilihan = "))
-    if b == 1:
+    print ("Menu :\n1. Input Nilai\n2. Bilangan genap\n3. Bilangan ganjil\n4. Exit")
+    a = int(input("Masukkan pilihan = "))
+    if a == 1:
         while True:
-            a = int(input("Masukkan nilai = "))
-            if a == 0:
+            b = int(input("Masukkan nilai = "))
+            if b == 0:
                 break
-            n.append(a)
-    elif b == 2:
+            n.append(b)
+    elif a == 2:
         for i in (n):
-            c += n
-        d = c / len(n)
-        print ("Rata rata nya adalah", d)
-    elif b == 3:
+            if i % 2 == 0:
+                c += 1
+        print ("Bilangan genap ada =", c)
+        c = 0
+    elif a == 3:
         for i in (n):
-            if n % 2 == 0:
-                e += 1
-        print ("Bilangan genap ada = ", e)
-        e = 0
-    elif b == 4:
-        for i in (n):
-            if n % 2 == 1:
-                f += 1
-        print ("Bilangan ganjil ada = ", f)
-        f = 0
-    elif b == 5:
+            if i % 2 == 1:
+                d += 1
+        print ("Bilangan ganjil ada =", d)
+        d = 0
+    elif a == 4:
         break
     else:
         print ("Input tidak benar")
